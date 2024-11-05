e8975872dcc1a2746ea492a67d77f4bd41f15d38
unknown <ganeshvelisoju919@gmail.com>
2024-11-05 22:15:45 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string userName;
+System.Console.WriteLine("Enter Username");
+userName = Console.ReadLine();
+System.Console.WriteLine("Welcome " +userName);
