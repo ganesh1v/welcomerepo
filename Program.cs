515965df6230c195199045c51dd2da56bfe49290
unknown <ganeshvelisoju919@gmail.com>
2024-11-05 22:31:19 +0530

    Second Commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string userName;
+System.Console.WriteLine("Welcome to .Net Core");
 System.Console.WriteLine("Enter Username");
 userName = Console.ReadLine();
 System.Console.WriteLine("Welcome " +userName);
